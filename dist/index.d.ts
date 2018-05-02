3ce0d7594ab0f643db2b09cf6c1cc5e7ec500422
Sienkiewicz Patryk <p.sienkiewicz@tme3c.com>
2018-05-02 10:09:09 +0200

    Added dist types,  moved types to index.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,35 @@
-import { AntConfig, AntResultEvent, AntSourceEvent } from "./types";
 import { Observable } from "@reactivex/rxjs";
+export interface AntSourceDefinition {
+    name: string;
+    modifiers?: Array<(...params: any[]) => any>;
+    toResult?: boolean;
+    ifMissing?: any;
+}
+export interface AntResultDefinitionPart {
+    name: string;
+    toResult?: boolean;
+    ifMissing?: any;
+    check?: () => boolean;
+}
+export interface AntResultDefinition {
+    check?: () => boolean;
+    parts: AntResultDefinitionPart[];
+    args: string[];
+    handler: (...params: any[]) => any;
+}
+export interface AntConfig {
+    sources: AntSourceDefinition[];
+    results: AntResultDefinition[];
+    additionalConfig?: any[];
+}
+export interface AntEvent {
+    name: string;
+    payload: any;
+}
+export interface AntSourceEvent extends AntEvent {
+}
+export interface AntResultEvent extends AntEvent {
+    toResult: boolean;
+}
 export declare const fromObservable: (source$: Observable<AntSourceEvent>, config: AntConfig) => Observable<AntResultEvent>;
 export declare const fromPromise: (source: Promise<AntSourceEvent[]>, config: AntConfig) => Observable<AntResultEvent>;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,42 @@
-import {
-  AntConfig, AntResultDefinition, AntResultDefinitionPart, AntResultEvent, AntSourceDefinition,
-  AntSourceEvent
-} from "./types";
 import { Observable, Subject } from "@reactivex/rxjs";
+
+export interface AntSourceDefinition {
+  name: string;
+  modifiers?: Array<(...params: any[]) => any>;
+  toResult?: boolean;
+  ifMissing?: any;
+}
+
+export interface AntResultDefinitionPart {
+  name: string;
+  toResult?: boolean;
+  ifMissing?: any;
+  check?: () => boolean;
+}
+
+export interface AntResultDefinition {
+  check?: () => boolean;
+  parts: AntResultDefinitionPart[],
+  args: string[];
+  handler: (...params: any[]) => any;
+}
+
+export interface AntConfig {
+  sources: AntSourceDefinition[];
+  results: AntResultDefinition[];
+  additionalConfig?: any[];
+}
+
+export interface AntEvent {
+  name: string;
+  payload: any;
+}
+
+export interface AntSourceEvent extends AntEvent {}
+
+export interface AntResultEvent extends AntEvent {
+  toResult: boolean;
+}
 
 export const fromObservable = (source$: Observable<AntSourceEvent>, config: AntConfig): Observable<AntResultEvent> => {
   // validate configs
